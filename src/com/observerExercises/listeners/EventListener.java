2df1c1be4b6c5ed6c5095900165a29787b91a098
Leonardo <leonardo38361@gmail.com>
2024-05-23 01:56:21 -0300

    patróng observer

---
+++ src/com/observerExercises/listeners/EventListener.java
@@ -0,0 +1,7 @@
+package com.observerExercises.listeners;
+
+import java.io.File;
+
+public interface EventListener {
+	void update(String eventType, File file);
+}
---
+++ src/com/observerExercises/test/Demo.java
@@ -0,0 +1,21 @@
+package com.observerExercises.test;
+
+import com.observerExercises.editor.Editor;
+import com.observerExercises.listeners.EmailNotificationListener;
+import com.observerExercises.listeners.LogOpenListener;
+
+public class Demo {
+	public static void main(String[] args) {
+		Editor editor = new Editor();
+
+		editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+		editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+		try {
+			editor.openFile("test.txt");
+			editor.saveFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
